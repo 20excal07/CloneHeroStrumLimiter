575ba3bbb5c95a4e1c9b1fd54f0424267123cdc7
Excal <20excal07@users.noreply.github.com>
2021-05-14 18:32:20 +0700

    Create preventDoubleStrum.py

---
+++ preventDoubleStrum.py
@@ -0,0 +1,26 @@
+import time
+
+if starting:
+	strumHoldPeriod = 0.033333		#seconds (default: 1/30th of a second)
+	vJoyId = 0
+	joyId = 1
+	
+	system.setThreadTiming(TimingTypes.HighresSystemTimer)
+	system.threadExecutionInterval = 1000 / 120
+	timepress = 0
+
+if keyboard.getPressed(Key.Space) is True:
+	vJoy[vJoyId].setPressed(0)
+
+for i in range(14):
+	vJoy[vJoyId].setButton(i, joystick[joyId].getDown(i))
+
+if joystick[joyId].pov[0] != -1:
+	timepress = time.clock()
+	vJoy[vJoyId].setAnalogPov(0, joystick[joyId].pov[0])
+
+if (timepress > 0 and time.clock() > timepress + strumHoldPeriod):
+	vJoy[vJoyId].setAnalogPov(0, -1)
+	timepress = 0
+
+vJoy[vJoyId].z = filters.mapRange(joystick[joyId].z,-1000,1000, 0,0x8000)
